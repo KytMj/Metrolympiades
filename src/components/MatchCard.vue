4439d945b10b6cd6c979d8cc272ace5e8fb3fa9f
jonas LE DUFF <jonasleduff14@gmail.com>
2025-04-18 20:40:00 +0200

    feat : match list functional but needs more style

---
+++ src/components/MatchCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+const props = defineProps({
+  match: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <article class="card">
+    <div>
+      <p class="activity">{{ match.activity }}</p>
+      <p class="date">{{ match.startedAt }}</p>
+    </div>
+    <div>
+      <h2 class="teamName">{{ match.team1 }} - {{ match.team1Score }}</h2>
+
+    </div>
+    <div>
+      <p class="score">{{ match.team2 }} - {{ match.team2Score }}</p>
+
+    </div>
+
+  </article>
+</template>
--- src/pages/GameListPage.vue
+++ src/pages/GameListPage.vue
@@ -1,17 +1,56 @@
 <script setup>
 import router from '@/router'
+import { ref } from 'vue'
+import MatchCard from '@/components/MatchCard.vue';
 
 function pushToAddGamePage() {
   router.push('/game')
 }
+
+const matches = ref([]);
+const isLoading = ref(false);
+const user = JSON.parse(localStorage.getItem("user"));
+
+function fetchMatches(){
+    isLoading.value = true;
+    fetch("http://localhost:3000/matches/me", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user.token}`,
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        matches.value = data;
+        console.log("matches.value = ",matches.value.at(0).activity);
+        isLoading.value = false;
+    });
+}
+fetchMatches();
+
+
 </script>
 
 <template>
   <main>
     <div class="header">
-      <h1>Mes matchs</h1>
+      <h1>Mes matches</h1>
       <div class="container">
         <button @click="pushToAddGamePage">Ajouter un match</button>
+      </div>
+    </div>
+    <div>
+      <h1>c'est les matches</h1>
+      <div v-if="isLoading">
+          <p>Chargement...</p>
+      </div>
+      <div v-else-if="matches.length === 0">
+          <p>Aucun classement disponible.</p>
+      </div>
+      <div v-else class="centerer">
+          <MatchCard v-for="match in matches" :key="match.id" :match = "match" class="card"/>
+
       </div>
     </div>
   </main>
